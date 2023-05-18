f4bcc1b61b9c2c171e7c0580b664f404fd06131c
aycakman <aycaakman0@gmail.com>
2023-05-18 15:04:00 +0300

    designed storyboard

---
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Ayca Akman on 17.05.2023.
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var searchtextField: UITextField!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+     
+
+    }
+}
+
+
---
+++ WeatherApp/Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by Ayca Akman on 18.05.2023.
+//
+
+import Foundation
