e9005e5efedaeb5be69e0557022b706093cf6b2a
aycakman <aycaakman0@gmail.com>
2023-05-22 08:36:06 +0300

    added CoreLocation to get current location data

--- WeatherApp/Controller/WeatherViewController.swift
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -5,8 +5,8 @@
 //  Created by Ayca Akman on 17.05.2023.
 
 import UIKit
-
-class WeatherViewController: UIViewController, WeatherManagerDelegate {
+import CoreLocation
+class WeatherViewController: UIViewController {
 
 
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -15,38 +15,31 @@ class WeatherViewController: UIViewController, WeatherManagerDelegate {
     @IBOutlet weak var conditionImageView: UIImageView!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
         weatherManager.delegate = self
         searchtextField.delegate = self
     }
-   
-    
+ 
+    @IBAction func currentLocationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+}
+
+//MARK: - UITextFieldDelegate
+extension WeatherViewController: UITextFieldDelegate {
     @IBAction func searchButtonPressed(_ sender: UIButton) {
         searchtextField.endEditing(true) // dismiss the keyboard
         print(searchtextField.text!)
 
     }
-    
-    func didUpdateWeather(_ weatherManager: WeatherManager, weatherModel: WeatherModel) {
-        DispatchQueue.main.async {
-            self.temperatureLabel.text = weatherModel.temperatureString
-            self.conditionImageView.image = UIImage(systemName: weatherModel.conditionName)
-            self.cityNameLabel.text = weatherModel.cityName
-
-        }
-    }
-    
-    func didFailWithError(error: Error) {
-        print(error)
-    }
-    
-}
-
-
-extension WeatherViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         searchtextField.endEditing(true)
@@ -69,3 +62,37 @@ extension WeatherViewController: UITextFieldDelegate {
         searchtextField.text = ""
     }
 }
+
+//MARK: - WeatherManagerDelegate
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weatherModel: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weatherModel.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weatherModel.conditionName)
+            self.cityNameLabel.text = weatherModel.cityName
+
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - CLLocationManagerDelegate
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
+
--- WeatherApp/Model/WeatherManager.swift
+++ WeatherApp/Model/WeatherManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 //delegate pattern
 protocol WeatherManagerDelegate {
@@ -21,6 +22,11 @@ struct WeatherManager {
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
         print(urlString)
+        performRequest(with: urlString)
+    }
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
         performRequest(with: urlString)
     }
     
